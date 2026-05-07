aaa2d97bc00ceee0283b0c9140e8af0fa75c59c9
Pepe.Otero <joterojordan@gmail.com>
2026-05-07 17:23:34 +0200

    feat: añadir PostgreSQL como tercer contenedor de base de datos
    Co-authored-by: Jaime Carrasco <9404633@alumnos.ufv.es>
    Co-authored-by: Jaime Escriva <9401438@alumnos.ufv.es>
    Co-authored-by: Hugo Dominguez <9402083@alumnos.ufv.es>
    Co-authored-by: César González <9407198@alumnos.ufv.es>

--- biblioteca/fastapi/app/database.py
+++ biblioteca/fastapi/app/database.py
@@ -1,29 +1,27 @@
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./biblioteca.db"
-
-engine = create_engine(
-
-    DATABASE_URL,
-
-    connect_args={"check_same_thread": False}
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@postgres:5432/biblioteca"
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
-def get_db():
 
+def get_db():
     db = SessionLocal()
 
     try:
-
         yield db
-
     finally:
-
         db.close()
